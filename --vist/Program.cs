56606d20f0bced607673ec4fa90c936f9d31e9df
Anton <tosha.golenko@yandex.ru>
2020-09-23 22:15:47 +0300

    1

---
+++ --vist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace __vist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
