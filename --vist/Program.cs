77bf2518c3bf1eff73cf3a2427e994bbd44e72a1
Anton <tosha.golenko@yandex.ru>
2020-09-23 22:22:39 +0300

    попытка 13

--- --vist/Program.cs
+++ --vist/Program.cs
@@ -6,7 +6,7 @@ namespace __vist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Hello friends!");
             Console.ReadLine();
         }
     }
